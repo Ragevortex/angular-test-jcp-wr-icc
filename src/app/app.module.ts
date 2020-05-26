76e8f80a8b8ce049afb3165aa3f9272e069af230
Ragevortex <peniaman@gmail.com>
2020-05-26 10:47:27 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FlexLayoutModule } from '@angular/flex-layout';
+//import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card.component';
 import { HelloComponent } from './hello.component';
 import { MatSliderModule } from '@angular/material/slider';
+import { FlexLayoutModule } from "@angular/flex-layout";
 @NgModule({
   imports:      [ BrowserModule, BrowserAnimationsModule, FlexLayoutModule, FormsModule, MatSliderModule],
   declarations: [ AppComponent, CardComponent, HelloComponent ],
